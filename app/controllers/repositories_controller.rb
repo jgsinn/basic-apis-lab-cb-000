f670d0366548cb56599bc2b9c17c6bd4e320c14b
Justin Sinn <justin.sinn.co@gmail.com>
2018-08-14 23:35:16 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,9 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    Faraday.get 'https://api.github.com/search/repositories?client_id=19a9083448ae524b3f07&client_secret=00a9e52652c48534ff6b2bb58489c3cba3bbc6ab' do |req|
+    Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['q']= 'recurly'
-
+      req.params['client_id']= '19a9083448ae524b3f07'
+      req.params['client_secret']= '00a9e52652c48534ff6b2bb58489c3cba3bbc6ab'
     end
   end
 end
